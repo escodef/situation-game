5e7d02be535c9a107ea2609302aea475f164cdce
Leon-baton <lalaha121@gmail.com>
2025-12-03 10:27:39 +0300

    sadfsadfsadf

--- src/bot/commands/start.ts
+++
@@ -1,9 +0,0 @@
-import { CommandContext, Context } from 'grammy';
-
-export default {
-    name: 'start',
-    description: 'говно',
-    execute: async (ctx: CommandContext<Context>) => {
-        await ctx.reply('соси хуй быдло');
-    },
-};
